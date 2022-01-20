5dc2366bdbbb51732a64343a5b86ab849e1e498a
Abdul Jabbar <41137069+ajfreelancer@users.noreply.github.com>
2022-01-20 10:51:31 +0500

    3 Section Built Responsively

---
+++ src/modules/FourthSection.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+imp
+
+const Div = styled.div`
+
+`
+
+const FourthSection = () => {
+    return (
+        <section>
+            <Div>
+                <div className="head-content">
+                    <h2>FEATURES</h2>
+                    <p>Most calendars are designed for teams. Slate is designed for freelancers who want a simple way to plan their schedule.</p>
+                </div>
+                <div className="grid">
+                    <div className="first-column">
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
+                </div>
+            </Div>
+        </section>
+    )
+}
+
+export default FourthSection
